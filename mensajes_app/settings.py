a90cb37260b510439c126aaac50f83ee43156c49
joan566 <joansebastianvergaravalencia@gmail.com>
2025-09-12 13:46:40 -0500

    coma en sttings

--- mensajes_app/settings.py
+++ mensajes_app/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mensajes_app.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default = "postgresql://postgres:postgres@localhost/postgres".
+        default = "postgresql://postgres:postgres@localhost/postgres",
         conn_max_age = 600
     )
 }
